3a607fec047e4b2accf29cbc2d7aa259eb538271
sabian77 <sabianrakasinau@gmail.com>
2025-04-20 18:36:56 +0700

    jwt susscess

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
 {
@@ -15,32 +14,30 @@ class LoginController extends Controller
         // validate request
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'password' => 'required|string|min:6',
+            'password' => 'required'
         ]);
-    
+
         // validation failed?
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
-    
+
         // get credentials
         $credentials = $request->only('email', 'password');
-    
+
         // authentication failed?
         if (!$token = auth()->guard('api')->attempt($credentials)) {
             return response()->json([
                 'success' => false,
-                'message' => 'Email atau Password Anda salah'
+                'message' => 'Email atau Password Salah'
             ], 401);
         }
-    
+
         // return response
         return response()->json([
             'success' => true,
             'user' => auth()->guard('api')->user(),
-            'token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => config('jwt.ttl') * 60
+            'token' => $token
         ], 200);
     }
 }
